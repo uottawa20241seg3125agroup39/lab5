8377b11453dbf26161f40625db7a21ddc5701a45
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2024-02-06 22:04:15 -0500

    appoint form optimize

--- js/appoint.js
+++ js/appoint.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     $('#appointment-btn').on('click', function() {
         console.log('Appointment button clicked...');
     })
+    $('#appoint-submit').on('click', function() {
+        console.log('Appointment form submitted...');
+    });
     // $('.modal').modal();
     console.log('Appointment Form Initialized...');
 });
