6337bb073048a96c791cf6245839c3fa7d352fdb
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2024-02-06 21:30:35 -0500

    WIP

---
+++ js/appoint.js
@@ -0,0 +1,8 @@
+/* global $, jQuery, bootstrap */
+$(document).ready(function() {
+    $('#appointment-btn').on('click', function() {
+        console.log('Appointment button clicked...');
+    })
+    // $('.modal').modal();
+    console.log('Appointment Form Initialized...');
+});
