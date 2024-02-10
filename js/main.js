61a4deac7acbe32dbdb1ec6446de9b1b37875f48
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2024-02-10 14:09:51 -0500

    optimize navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
+// noinspection SpellCheckingInspection
+
 $('#document').ready(function() {
+  rickroll_empty_href();
   console.log('Initialized...');
 });
+
+function rickroll_empty_href() {
+  // console.log($('a[href=""], a[href=undefined], a[href="#"]'));
+  $('a[href=""], a[href=undefined], a[href="#"]').each(function() {
+    this.href = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    // console.log(a);
+    // console.log($(a));
+    $(this).attr('title','Try to click?').attr('target','_blank');
+  })
+  console.log('Rickroll set')
+}
