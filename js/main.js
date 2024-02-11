2e75a9da2fbf0522eaeb4c6b652cdfc524cab88c
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2024-02-11 14:19:16 -0500

    appointment form

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@
 $('#document').ready(function() {
   rickroll_empty_href();
   set_popup_a();
+  load_appt_form();
   console.log('Initialized...');
 });
 
@@ -22,4 +23,34 @@ function set_popup_a(){
         $(this).attr('target', '_blank');
     })
     console.log('Popup set')
+}
+
+function load_appt_form(){
+  $('#appt-modal').ready(function() {
+    $.ajax({
+        url:'/appointment.html',
+        type:'GET',
+        dataType:'html',
+        success:function(data){
+            let code=$(data).find('#modal-appointment');
+            $('#appt-modal').html(code);
+            console.log('Appointment Form Loaded');
+        },
+        error:function(){
+          $('#appt-modal').html('<div class="alert alert-danger" role="alert">Error loading appointment form. Refresh your page or contact the IT.</div>');
+            console.log('Error loading Appointment Form');
+        },
+        complete:function(){
+        $('.appt-btn').each(function() {
+          // $(this).ready(function() {
+            // $(this).data('toggle','modal').data('target','#appt-modal');
+            // console.log($(this).data())
+            $(this).attr('data-toggle','modal').attr('data-target','#appt-modal');
+            // $(this).html('test')
+          // })
+        })
+          console.log('Appointment button set')
+      },
+    })
+  })
 }
