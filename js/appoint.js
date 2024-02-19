5a2a500b5f7048070a98b0f20a7c7fcd313e4560
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2024-02-18 19:22:33 -0500

    Framework compatibilization-fix bug

--- js/appoint.js
+++ js/appoint.js
@@ -6,6 +6,23 @@ $(document).ready(function() {
     $('#appoint-submit').on('click', function() {
         console.log('Appointment form submitted...');
     });
+    $('#appointment-form-body').submit(function (e){
+        e.preventDefault();
+        appointmentNextButton();
+    });
     // $('.modal').modal();
     console.log('Appointment Form Initialized...');
 });
+
+function canIUse(func){
+    // function definition check code from GPT4
+    return typeof  window[func] === 'function'
+}
+
+function appointmentNextButton(){
+    if (canIUse('global_goPayment')){
+        global_goPayment();
+        return;
+    }
+    console.log('Cannot find uplevel function');
+}
